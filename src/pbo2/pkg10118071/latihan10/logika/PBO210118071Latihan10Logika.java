759a2782266b0f470eec1e0e9afc6242638d603d
David Aditya Winarto <David Aditya Winarto@LAPTOP-1HV2CE3U>
2019-09-22 00:36:41 +0700

    PBO2-10118071-Latihan10-Logika

---
+++ src/pbo2/pkg10118071/latihan10/logika/PBO210118071Latihan10Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118071.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA              : David Aditya Winarto
+ * KELAS             : PBO2
+ * NIM               : 10118071
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ *                     tipe data logic true atau false. high atau low, 1 atau 0, benar atau salah
+ * 
+ */
+public class PBO210118071Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+    
+}
